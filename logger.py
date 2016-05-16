1510fad6399c12fbeb95451ce6452528ba647f5a
Steven Tompkins <steven.tompkins.jr@gmail.com>
2016-05-15 20:21:48 -0500

    Added logging utility to utilize python logging, modified root mapping to redirect to index

--- logger.py
+++ logger.py
@@ -4,13 +4,24 @@ class Logger(object):
     """docstring for Logger"""
     def __init__(self):
         super(Logger, self).__init__()
-	try:
-	    import http.client as http_client
-	except ImportError:
-	    # Python 2
-	    import httplib as http_client
-	http_client.HTTPConnection.debuglevel = 1
-        logging.basicConfig()
-	logging.getLogger().setLevel(logging.DEBUG)
+    try:
+        import http.client as http_client
+    except ImportError:
+        # Python 2
+        import httplib as http_client
+    http_client.HTTPConnection.debuglevel = 1
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.DEBUG)
 
-    def warn()
+
+    def warn(self, msg):
+        logging.warn(msg, logging.WARN)
+
+    def error(self, msg):
+        logging.error(msg, logging.ERROR)
+
+    def info(self, msg):
+        logging.info(msg, logging.INFO)
+
+    def debug(self):
+        logging.debug(msg, logging.DEBUG)
--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    return index()
 
 
 @app.route('/me/index.html')
